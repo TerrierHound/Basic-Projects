b69367026154d451f99aab22181d9f40f2d90bfa
Ata Erdemir <erdemir.ata@gmail.com>
2019-10-06 20:08:24 +0200

    Login Screen with Python

---
+++ Login Screen.py
@@ -0,0 +1,12 @@
+print("Welcome to HELL !!!")
+
+Kullanici_adi = "Ayberk"
+Parola = "gotayberk"
+
+Gecerli_Kullanici = input("Kullanici adi: ")
+Gecerli_Parola = input("Parola: ")
+
+if Gecerli_Kullanici == Kullanici_adi and Gecerli_Parola == Parola:
+    print("Girdiginiz bilgiler gereçlidir. Giriş yaptınız ! Tebrikler !")
+else:
+    print("Kullanici adi veya Parola yanlış")
